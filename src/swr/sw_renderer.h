6505eb2c3428aa6e4aa35863f3676f66a54201cd
iProgramInCpp <iprogramincpp@gmail.com>
2026-05-15 19:15:39 +0300

    Revert "* BIG CHANGE: Allow for primitive difference tracking."
    This reverts commit d42d869d8cc1082acc15e63e9c9109a58f69bb49.

--- src/swr/sw_renderer.h
+++ src/swr/sw_renderer.h
@@ -1,22 +1,7 @@
 #pragma once
 
-#include <stdint.h>
 #include "renderer.h"
-
-typedef struct {
-	int16_t x1, y1, x2, y2;
-}
-SWRectangle;
-
-#define swrSetRect(rc, _x1, _y1, _x2, _y2) do { \
-	(rc)->x1 = (_x1); \
-	(rc)->y1 = (_y1); \
-	(rc)->x2 = (_x2); \
-	(rc)->y2 = (_y2); \
-} while (0)
 
 Renderer* SWRenderer_create(int windowWidth, int windowHeight);
 
 void SWRenderer_clearFrameBuffer(Renderer* renderer, uint32_t color);
-
-SWRectangle* SWRenderer_getUpdateRects(Renderer* renderer, int* rectCount, bool* overflow);
