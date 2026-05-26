82f78aa0f3cb10da7480a29842bfdd9bc12d8a82
Un1q32 <joey.t.reinhart@gmail.com>
2026-05-26 05:15:22 -0400

    also no reason to zero memory here

--- src/sw/sw_renderer.c
+++ src/sw/sw_renderer.c
@@ -222,7 +222,7 @@ FORCE_INLINE int swrCeiling(float x)
 static SWTexture* swrCreateTexture(const uint8_t* srcBuffer, int width, int height)
 {
 	SWTexture* txt = safeCalloc(1, sizeof(SWTexture));
-	txt->buffer = safeCalloc(width * height, sizeof(uintpixel_t));
+	txt->buffer = safeMalloc(width * height * sizeof(uintpixel_t));
 	
 	const uint32_t* rgbaSrc = (const uint32_t*) srcBuffer;
 
